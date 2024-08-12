ef0b3e88e054c0ea22a5ffb60765fa2720b70648
kanakanho <kahoshibakichi@gmail.com>
2024-08-12 12:35:23 +0900

    Initial Commit

---
+++ PlayPianoiPad/PlayPianoiPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayPianoiPadApp.swift
+//  PlayPianoiPad
+//
+//  Created by blue ken on 2024/08/12.
+//
+
+import SwiftUI
+
+@main
+struct PlayPianoiPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
